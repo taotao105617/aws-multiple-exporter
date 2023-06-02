9892e1c35632301c8ec8fe63475d6fd605e4238d
刘涛 <tao133.liu@tcl.com>
2023-06-02 16:38:02 +0800

    ci: 抽离配置到configs.yaml

--- conf/configs.py
+++ conf/configs.py
@@ -1,15 +1,7 @@
 from lib.yaml_reader import YamlReader
+
 logs_dir = './logs'
-# 环境
-# env = 'online'
-# # 启动端口
-# address_ports = {
-#     'redis': 8888
-# }
-# redis 配置
-# redis_address = '127.0.0.1'
-# redis_ports = 6379
-# redis_password = None
+
 cache_invalidation_time = 1000
 
 
@@ -22,18 +14,12 @@ ProductNamespaceF = {
     'redis': {
         'namespace': 'AWS/ElastiCache',
         'filter': 'CacheClusterId'
-    }
+    },
+    'rds': {
+        'namespace': 'AWS/RDS',
+        'filter': 'DBInstanceIdentifier'
+    },
 }
-
-# aws 账号信息
-# aws_account_infos = [
-#     {
-#         'account': 'xxx',
-#         'ak': 'xxxx',
-#         'sk': 'xxxx',
-#         'regions': 'us-east-1,ap-southeast-1,eu-central-1,sa-east-1'
-#     }
-# ]
 
 
 class Configs:
@@ -46,5 +32,4 @@ class Configs:
 
 
 configs = Configs()
-
 
